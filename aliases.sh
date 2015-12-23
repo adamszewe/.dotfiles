78d2cecfb1ed37958b7b174dcd9ae6f11b631626
Adam <adam.szewe@gmail.com>
2015-12-23 22:12:31 +0100

    Modifies tmux.conf - this configuration is more than enough for me

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 # Description: Contains all the aliases used most commonly.
 
-
 alias l='ls -CF'
 alias ll='ls -l'
 alias la='ls -la'
--- install.sh
+++ install.sh
@@ -1,3 +1,23 @@
 
 
 # copy the 
+
+files=(
+    bashrc 
+    conkyrc 
+    gitconfig 
+    tmux.conf 
+    vimrc 
+    zshrc
+)
+
+for dotfile in $files
+do
+    if [ -h "~/.$dotfile" ] 
+    then 
+        echo "$dotfile already exists"
+    else 
+        ln -s "/home/adam/.dotfiles/$dotfile" "/home/adam/.$dotfile"
+    fi
+done
+
