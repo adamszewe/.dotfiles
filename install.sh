df60d33ef7ccd09609bf9283510ba05cdffc49b9
Adam Szewera <adam.szewe@gmail.com>
2026-01-07 11:48:39 +0100

    Remove hammerspoon configuration from dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
 stow vim
 stow shell
 stow tmux
-
-if [[ $(uname -s) -eq "Darwin" ]]; then
-  stow hammerspoon
-fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,3 @@
 stow -D vim
 stow -D shell
 stow -D tmux
-
-if [[ $(uname -s) -eq "Darwin" ]]; then
-  stow hammerspoon
-fi
